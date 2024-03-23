dbf91dbea88ea9786a9ff07a8e719998cb0b8e8e
Sebastian Guerrero <msebgf@yahoo.com>
2024-03-23 01:45:19 +0000

    add timer

--- app/src/main/java/com/example/mobile1/PersonsList.kt
+++ app/src/main/java/com/example/mobile1/PersonsList.kt
@@ -7,17 +7,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -29,9 +33,27 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 
 @Composable
 fun PersonsList() {
+
+    var timeLeft by remember {
+        mutableIntStateOf(10)
+    }
+
+    var isPaused by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(key1 = timeLeft) {
+        while (timeLeft > 0 && !isPaused ) {
+            delay(1000L)
+            timeLeft --
+        }
+    }
+
     var name by remember {
         mutableStateOf("")
     }
@@ -49,6 +71,43 @@ fun PersonsList() {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Time left: ${timeLeft.toString()}",
+                modifier = Modifier
+                    .padding(16.dp),
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                modifier = Modifier.padding(16.dp),
+                onClick = {
+                    isPaused = true
+                }) {
+                Icon(
+                    modifier = Modifier
+                        .size(20.dp),
+                    imageVector = Icons.Default.Warning,
+                    contentDescription = null
+                )
+            }
+            Button(
+                modifier = Modifier.padding(16.dp),
+                onClick = {
+                timeLeft = 10
+            }) {
+                Icon(
+                    modifier = Modifier
+                        .size(20.dp),
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = null
+                )
+            }
+        }
 
         TextField(value = name,
             modifier = Modifier
